abfa3ab09fc0ebff77dc957d21e6e805de7f5851
georgeCx5 <69874793+georgeCx5@users.noreply.github.com>
2023-07-25 14:52:14 -0500

    Finished ✅
    - Mobile ✔
    - Tablet ✔
    - Desktop ✔

--- views/HomeView.vue
+++ views/HomeView.vue
@@ -1,6 +1,7 @@
 <script>
 import RouterBtn from '../components/RouterBtn.vue';
 import FooterComponent from '../components/FooterComponent.vue';
+import ModernHeading from '../components/ModernHeading.vue';
 
 export default {
    data() {
@@ -31,6 +32,7 @@ export default {
    },
    components: {
       RouterBtn,
+      ModernHeading,
       FooterComponent,
    },
    methods: {
@@ -45,19 +47,26 @@ export default {
 }
 </script>
 <template>
-   <body class=" flex flex-col items-center gap-[120px] text-neo-black font-shoulders">
+   <body class=" flex flex-col items-center gap-[120px] DT:gap-[180px] text-neo-black font-shoulders">
       <header
-         class=" relative flex flex-col TB:flex-row TB:justify-end TB:items-center gap-8 w-full max-w-[425px] TB:max-w-[1024px] DT:max-w-[1920px] TB:h-[700px]">
-         <div
-            :class="` TB:absolute TB:left-0 TB:top-0 w-full TB:w-[57%] h-[240px] TB:h-full ${setBackground(imgData[0])} bg-cover bg-center`">
+         class=" relative flex flex-col TB:flex-row TB:justify-end TB:items-center gap-8 w-full max-w-[425px] TB:max-w-[1024px] DT:max-w-[1920px] TB:h-[700px] DT:h-[800px]">
+         <div class=" DT:absolute left-0 hidden DT:flex items-center w-[31.25%] h-full bg-neo-black overflow-hidden">
+            <ModernHeading :is-main-layer="true" />
          </div>
-         <div class=" TB:relative flex flex-col gap-8 TB:gap-12 TB:w-[55%] px-4 TB:px-10">
-            <div class=" text-[60px] leading-[55px] font-black uppercase">
+         <ModernHeading :is-main-layer="false" />
+         <div
+            :class="` TB:absolute TB:left-0 DT:left-[31.25%] TB:top-0 DT:flex items-center w-full TB:w-[57%] DT:w-[540px] h-[240px] TB:h-full ${setBackground(imgData[0])} bg-cover bg-center overflow-hidden`">
+         </div>
+         <div
+            class=" TB:relative flex flex-col DT:flex-row DT:justify-between gap-8 TB:gap-12 TB:w-[55%] DT:w-full DT:h-[296px] DT:-mt-32 px-4 TB:px-10 DT:px-[170px]">
+            <div
+               class=" DT:text-transparent text-[60px] DT:text-[96px] leading-[55px] DT:leading-[88px] font-black uppercase">
                <h1>Modern</h1>
                <h1>Art Gallery</h1>
             </div>
-            <div class=" flex flex-col gap-8 TB:gap-12">
-               <p class=" text-neo-gray text-[18px] leading-[28px] font-outfit font-light">
+            <div class=" flex flex-col gap-8 TB:gap-12 DT:gap-16 DT:w-[32%]">
+               <p
+                  class=" text-neo-gray text-[18px] DT:text-[22px] leading-[28px] DT:leading-[32px] font-outfit font-light">
                   The arts in the collection of the Modern Art Gallery all started
                   from a spark of inspiration. Will these pieces inspire you? Visit
                   us and find out.
@@ -86,20 +95,20 @@ export default {
                </p>
             </div>
          </div>
-         <div class=" flex flex-col TB:flex-row gap-4 TB:gap-3">
+         <div class=" flex flex-col TB:flex-row gap-4 TB:gap-3 DT:gap-[30px]">
             <div :class="` w-full h-[480px] TB:h-[720px] ${setBackground(imgData[2])} bg-cover bg-center`">
             </div>
-            <div class=" flex flex-col items-start  gap-4 TB:gap-3 w-full TB:w-[70%]">
+            <div class=" flex flex-col items-start gap-4 TB:gap-3 DT:gap-[30px] w-full TB:w-[70%]">
                <div
                   :class="` w-full h-[200px] TB:h-[80%] ${setBackground(imgData[3])} bg-cover bg-bottom TB:bg-right-bottom`">
                </div>
                <div
-                  class=" flex flex-col justify-center gap-6 h-[293px] TB:h-full px-6 TB:px-[30px] bg-neo-black text-neo-white">
-                  <div class=" text-[50px] leading-[45px] font-black uppercase">
+                  class=" flex flex-col justify-center gap-6 DT:gap-8 h-[293px] TB:h-full px-6 TB:px-[30px] DT:px-12 bg-neo-black text-neo-white">
+                  <div class=" text-[50px] DT:text-[60px] leading-[45px] DT:leading-[60px] font-black uppercase">
                      <h2>Come &amp; be</h2>
                      <h2>inspired</h2>
                   </div>
-                  <p class=" text-[18px] leading-[28px] font-outfit font-light">
+                  <p class=" text-[18px] DT:text-[22px] leading-[28px] DT:leading-[32px] font-outfit font-light">
                      We’re excited to welcome you to our gallery and see how our collections
                      influence you.
                   </p>
